03f1db3bc18898888391f68f49c79f0dac510097
DmtrDv <dmitriybychkov75146@gmail.com>
2024-11-06 22:26:41 +0300

    Создал новый производный класс Discount_Juce для сока и вывел скидку

--- hierarchy/Discount Juce.cs
+++ hierarchy/Discount Juce.cs
@@ -6,20 +6,20 @@ using System.Threading.Tasks;
 
 namespace hierarchy
 {
-    class Discount_Energi : drinks
+    class Discount_Juce: drinks
     {
-        public Discount_Energi(string Name, double Cost, int Quantity) : base(Name, Cost, Quantity)
-        {                   
+        public Discount_Juce(string Name, double Cost, int Quantity) : base(Name, Cost, Quantity)
+        {
             if (Quantity > 5 && Quantity <= 10)
-             discount = "Скидка 10%";
+                discount = "Скидка 11%";
             if (Quantity > 10 && Quantity <= 20)
-                discount = "Скидка 20%";
+                discount = "Скидка 22%";
             if (Quantity > 20)
-                discount = "Скидка 30%";                            
+                discount = "Скидка 33%";
         }
 
         public override string Discount()
-        { 
+        {
             Console.WriteLine(discount);
             return discount;
         }
--- hierarchy/Program.cs
+++ hierarchy/Program.cs
@@ -10,14 +10,15 @@ namespace hierarchy
     {
         static void Main(string[] args)
         {
-            DiscountEnergi Energi_Burn = new DiscountEnergi("Burn ",100.0, 7);
+            Discount_Energi Energi_Burn = new Discount_Energi("Burn  ",100.0, 7);
             Console.Write(Energi_Burn.GetName());
             Console.WriteLine(Energi_Burn.GetCost());
             Energi_Burn.Discount();
 
-            drinks Juce_Rich = new drinks("Rich", 150.0, 12);
-            Console.WriteLine(Juce_Rich.GetName());
+            Discount_Juce Juce_Rich = new Discount_Juce("Rich  ", 150.0, 12);
+            Console.Write(Juce_Rich.GetName());
             Console.WriteLine(Juce_Rich.GetCost());
+            Juce_Rich.Discount();
 
 
             Console.ReadKey();
